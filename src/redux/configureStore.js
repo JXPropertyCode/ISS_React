1a41a89cd32ba79fb1e687c14561e22c3aaebad2
JXPropertyCode <yellowdice92@hotmail.com>
2021-07-17 16:20:07 -0700

    added build

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,8 +3,15 @@ import issSliceReducer from "./issSlice";
 import logger from "redux-logger";
 import issMiddleware from "../reduxMiddleware/issMiddleware";
 
+
+let middlewares = [issMiddleware]
+
+if (process.env.NODE_ENV === `development`) {
+	middlewares.push(logger);
+}
+
 export default configureStore({
-	middleware: [issMiddleware, logger],
+	middleware: middlewares,
 	reducer: {
 		iss: issSliceReducer,
 	},
